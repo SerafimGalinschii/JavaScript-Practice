15f5d4cec4ed928d7da3057d155616421432692f
Serafim Galinschii <serafim.galinschii@gmail.com>
2024-03-03 17:11:31 +0200

    remove comments from ex8

--- ex8.js
+++ ex8.js
@@ -1,28 +1,32 @@
+// Write a JavaScript program where the program takes a random integer between 1 and 10,
+//  and the user is then prompted to input a guess number. The program displays a message "Good Work" 
+//  if the input matches the guess number otherwise "Not matched".
+
 function getPlayerNumber() {
     // Generate a random number between 0 and 9 for the computer's move
     const computerMove = Math.floor(Math.random() * 10);
     console.log(computerMove);
 
-    // Get the text content of the HTML element with the id "target" as the player's move
+    
     const move = document.querySelector(".target");
 
     const playerMove = move.value;
 
 
 
-    // Initialize the 'result' variable
+    
     let result = '';
     console.log("Player's Move:", playerMove);
     console.log("Computer's Move:", computerMove);
 
 
-    // Check if the computer's move is equal to the player's move
+    
     if (parseInt(computerMove) === parseInt(playerMove)) {
         result = 'You win';
     } else {
         result = 'You lose';
     }
 
-    // Display the result in an HTML element with the class "result"
+    
     document.querySelector(".result").innerHTML = result;
 }
